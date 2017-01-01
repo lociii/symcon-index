a0504fb5bcf28998b9439beed480b4b03d0fc1fc
Jens Nistler <opensource@jensnistler.de>
2017-01-01 19:11:26 +0100

    change celery task discovery, refs #2

--- heroku/celery.py
+++ heroku/celery.py
@@ -2,14 +2,13 @@
 import os
 from celery import Celery
 from celery.schedules import crontab
-from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heroku.settings')
 
 app = Celery('heroku')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'symcon_repository_update_nightly': {
         'task': 'symcon.tasks.symcon_repository_update_nightly',
